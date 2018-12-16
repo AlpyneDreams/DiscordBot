c19d665107a23c20f716b7d234a3f424b45366e8
DankParrot <dankparrot1@gmail.com>
2018-12-15 19:41:52 -0800

    Update batch files

--- start_selfbot.bat
+++ start_selfbot.bat
@@ -2,5 +2,5 @@
 title Bot
 set PATH=%PATH%;C:\Users\jeffr\Documents\GitHub\Discord\ffmpeg\bin
 :: node [--inspect] <config>
-node index configs/config.hjson
+node index configs/config_selfbot.hjson
 pause

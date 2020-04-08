a48d0fd53ce5309779e5b4822933008a80431d20
DankParrot <dankparrot1@gmail.com>
2020-04-08 01:15:11 -0700

    Fix signal handling

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require("./bot/util/console.js")
 const DiscordBot = require("./bot/DiscordBot.js")
-require('./bot/util/signals.js')(bot)
 
 var bot = new DiscordBot(process.argv[2] || 'configs/config.hjson')
+
+require('./bot/util/signals.js')(bot)
 
 module.exports = bot
 

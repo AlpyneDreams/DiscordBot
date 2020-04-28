7e8c99489e00c13ae1c0a7513dad94d1674c6bc7
DankParrot <dankparrot1@gmail.com>
2020-04-27 21:43:08 -0700

    --bots command, lists bots on a server

---
+++ modules/bots.js
@@ -0,0 +1,17 @@
+module.exports.commands = {
+
+    "bots": function(e) {
+
+        let members = e.guild.members
+        console.log(`[Bots] Fetched ${members.size}/${e.guild.memberCount} members from guild ${e.guild.name}`)
+
+        let bots = members.filter(m => m.user.bot)
+        console.log(`[Bots] Filtered ${bots.size} bots from guild ${e.guild.name}`)
+
+        let msg = bots.map(b => b.toString()).join('\n')
+
+        e.channel.send(msg)
+
+    }
+
+}
